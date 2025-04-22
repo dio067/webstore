967498882d28c40c65cf86e9dbdc074574d03845
dio067 <yousefnas06@gmail.com>
2025-04-23 00:09:56 +0400

    feat: use bodyParser as middleware function

--- index.js
+++ index.js
@@ -13,16 +13,24 @@ app.get("/", (req, res) => {
         </div>`);
 });
 
+const bodyParser = (req, res, next) => {
+	if (req.method === "POST") {
+		req.on("data", (data) => {
+			const parsed = data.toString("utf8").split("$");
+			const formData = {};
+			for (let pair of parsed) {
+				const [key, value] = pair.split("=");
+				formData[key] = value;
+			}
+			req.body = formData;
+			next();
+		});
+	} else {
+		next();
+	}
+	res.send("Account Created");
+};
 app.post("/", (req, res) => {
-	req.on("data", (data) => {
-		const parsed = data.toString("utf8").split("$");
-		const formData = {};
-		for (let pair of parsed) {
-			const [key, value] = pair.split("=");
-			formData[key] = value;
-		}
-		console.log(formData);
-	});
 	res.send("Account Created");
 });
 app.listen(4000, () => {

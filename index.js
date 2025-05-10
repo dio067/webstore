0255cfa4f0f36131dfe8b75512723df2610fa7ac
dio067 <yousefnas06@gmail.com>
2025-05-10 19:25:05 +0400

    feat: add cookies sessions

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import usersRepo from "./repositories/users.js";
+import cookieSession from "cookie-session";
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
+app.use(
+	cookieSession({
+		keys: ["ksdajfkjjdflkajsd"],
+	})
+);
+app.get("/signup", (req, res) => {
 	res.send(`<div>
         <form method="POST">
         <input name="email" placeholder="email">
@@ -15,7 +20,7 @@ app.get("/", (req, res) => {
         </form>
         </div>`);
 });
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
 	const { email, password, passwordConfirmation } = req.body;
 	const existingUser = await usersRepo.getOneBy({ email });
 	if (existingUser) {
@@ -24,6 +29,13 @@ app.post("/", async (req, res) => {
 	if (password !== passwordConfirmation) {
 		return res.send("passwords must match");
 	}
+
+	// Create a user in the users repo
+	const user = await usersRepo.create({ email, password });
+
+	// Store the id of the user inside the cookie
+	req.session.userId = user.id;
+
 	res.send("Account Created");
 });
 app.listen(4000, () => {

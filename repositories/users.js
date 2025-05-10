a29aaa8f6c44ca397859bc36c5d37f6496c6f7ff
dio067 <yousefnas06@gmail.com>
2025-05-10 12:02:28 +0400

    fix: edit logic on deleting records

--- repositories/users.js
+++ repositories/users.js
@@ -1,7 +1,5 @@
 import fs from "fs";
 import crypto from "crypto";
-import { stringify } from "querystring";
-import { json } from "stream/consumers";
 
 class UsersRepository {
 	constructor(filename) {
@@ -31,10 +29,23 @@ class UsersRepository {
 		attrs.id = this.randomId();
 		records.push(attrs);
 		this.writeAll(records);
+		return records;
 	}
 
 	randomId() {
 		return crypto.randomBytes(4).toString("hex");
+	}
+
+	async deleteById(id) {
+		const records = await this.getAll();
+		const filteredRecords = [];
+		for (let record of records) {
+			if (record.id !== id) {
+				filteredRecords.push(record);
+			}
+		}
+		this.writeAll(filteredRecords);
+		return filteredRecords;
 	}
 
 	async getOne(id) {
@@ -45,6 +56,7 @@ class UsersRepository {
 			}
 		}
 	}
+
 	async writeAll(records) {
 		await fs.promises.writeFile(
 			this.filename,
@@ -54,9 +66,13 @@ class UsersRepository {
 }
 const test = async () => {
 	const repo = new UsersRepository("users.json");
-	const user = await repo.getOne("423bcec2");
+	// const users = await repo.createUser({
+	// 	email: "nothing@nothing.com",
+	// 	password: "nothing",
+	// });
+	const deleted = await repo.deleteById("49c5a2c9");
 
-	console.log(user);
+	console.log(deleted);
 };
 
 test();

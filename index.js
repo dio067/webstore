db430c26bd0020a8685c359ceaa635b47676261f
dio067 <yousefnas06@gmail.com>
2025-05-11 12:48:12 +0400

    feat: add sign in and out pages

--- index.js
+++ index.js
@@ -38,6 +38,37 @@ app.post("/signup", async (req, res) => {
 
 	res.send("Account Created");
 });
+
+app.get("/signout", (req, res) => {
+	req.session = null;
+	res.send("Your logged out");
+});
+
+app.get("/signin", (req, res) => {
+	res.send(`<div>
+        <form method="POST">
+        <input name="email" placeholder="email">
+        <input name="password" placeholder="password">
+        <button type="submit" >Sign In</button>
+        </form>
+        </div>`);
+});
+
+app.post("/signin", async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await usersRepo.getOneBy({ email, password });
+
+	if (!user) {
+		res.send("User not found");
+	}
+
+	if (user.password != password) {
+		res.send("Invaild Password");
+	}
+	res.send("Your signed in");
+});
+app;
 app.listen(4000, () => {
 	console.log("Listening..");
 });

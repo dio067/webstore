4edaf163a87364147cea9dbc48f76f75aca0072b
dio067 <yousefnas06@gmail.com>
2025-05-12 01:26:58 +0400

    feat: add products routes

---
+++ routes/admin/products.js
@@ -0,0 +1,7 @@
+import express from "express";
+const router = express.Router();
+
+router.get("admin/products", (req, res) => {});
+router.get("/admin/products/new", (req, res) => {});
+
+export default router;

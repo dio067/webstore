c2be8803fb6510bc5f0bb948e2a16626e72585e5
dio067 <yousefnas06@gmail.com>
2025-05-12 15:12:13 +0400

    feat: create and find cart in repo

---
+++ routes/carts.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cartsRepo from "../repositories/carts.js";
+import carts from "../repositories/carts.js";
+const router = express.Router();
+
+// Receive a POST request to add an item to a cart
+router.post("/cart/products", async (req, res) => {
+	// Which Cart?
+	let cart;
+
+	if (!req.session.cartId) {
+		// In case we don't have one create one and assign the id of a cart as a cookie session id
+		cart = await cartsRepo.create({ items: [] });
+		req.session.cartId = cart.id;
+	} else {
+		// In Case we have a cart: Find it from the cart Repository
+		cart = await cartsRepo.getOne(req.session.cartId);
+	}
+
+	res.send("Product added");
+});
+
+// Receive a GET request to show all items in a cart
+
+// Recive a POST request to remove an item from a cart
+
+export default router;

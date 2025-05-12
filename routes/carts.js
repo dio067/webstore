b819a5391c53174f8f69ceb1edf7a7de43d82bbb
dio067 <yousefnas06@gmail.com>
2025-05-12 15:51:01 +0400

    feat: display items on the cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,43 +1,59 @@
 import express from "express";
 import cartsRepo from "../repositories/carts.js";
-import carts from "../repositories/carts.js";
+import productsRepo from "../repositories/products.js";
+import cartShowTemplate from "../view/carts/show.js";
+
 const router = express.Router();
 
-// Receive a POST request to add an item to a cart
+// Receive a post request to add an item to a cart
 router.post("/cart/products", async (req, res) => {
-	// Which Cart?
+	// Figure out the cart!
 	let cart;
-
 	if (!req.session.cartId) {
-		// In case we don't have one create one and assign the id of a cart as a cookie session id
+		// We dont have a cart, we need to create one,
+		// and store the cart id on the req.session.cartId
+		// property
 		cart = await cartsRepo.create({ items: [] });
 		req.session.cartId = cart.id;
 	} else {
-		// In Case we have a cart: Find it from the cart Repository
+		// We have a cart! Lets get it from the repository
 		cart = await cartsRepo.getOne(req.session.cartId);
 	}
 
-	// Whether add new to a cart or increment the quantity
-	const exisistingItem = cart.items.find(
+	const existingItem = cart.items.find(
 		(item) => item.id === req.body.productId
 	);
-	if (exisistingItem) {
-		// Increment
-		exisistingItem.quantity++;
+	if (existingItem) {
+		// increment quantity and save cart
+		existingItem.quantity++;
 	} else {
-		// Add new Item
+		// add new product id to items array
 		cart.items.push({ id: req.body.productId, quantity: 1 });
 	}
-
 	await cartsRepo.update(cart.id, {
 		items: cart.items,
 	});
 
-	res.send("Product added");
+	res.send("Product added to cart");
 });
 
-// Receive a GET request to show all items in a cart
+// Receive a GET request to show all items in cart
+router.get("/cart", async (req, res) => {
+	if (!req.session.cartId) {
+		return res.redirect("/");
+	}
 
-// Recive a POST request to remove an item from a cart
+	const cart = await cartsRepo.getOne(req.session.cartId);
+
+	for (let item of cart.items) {
+		const product = await productsRepo.getOne(item.id);
+
+		item.product = product;
+	}
+
+	res.send(cartShowTemplate({ items: cart.items }));
+});
+
+// Receive a post request to delete an item from a cart
 
 export default router;

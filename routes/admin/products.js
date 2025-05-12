147ee4f7767204bb777a0eae0c061fa88855484c
dio067 <yousefnas06@gmail.com>
2025-05-12 13:01:10 +0400

    feat: edit products list

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,6 +3,7 @@ import { check, validationResult } from "express-validator";
 import multer from "multer";
 import productsRepo from "../../repositories/products.js";
 import productsNewTemplate from "../../view/admin/products/new.js";
+import productsEditTemplate from "../../view/admin/products/edit.js";
 import validtors from "./validtors.js";
 import middlewares from "./middlewares.js";
 import productsIndexTemplate from "../../view/admin/products/index.js";
@@ -32,6 +33,42 @@ router.post(
 		await productsRepo.create({ title, price, image });
 
 		res.redirect("/admin/products");
+	}
+);
+
+router.get(
+	"/admin/products/:id/edit",
+	middlewares.requireAuth,
+	async (req, res) => {
+		const product = await productsRepo.getOne(req.params.id);
+
+		if (!product) {
+			return res.send("Product not found");
+		}
+
+		res.send(productsEditTemplate({ product }));
+	}
+);
+
+router.post(
+	"/admin/products/:id/edit",
+	middlewares.requireAuth,
+	upload.single("image"),
+	[validtors.requireTitle, validtors.requirePrice],
+	middlewares.handleErrors(productsEditTemplate),
+	async (req, res) => {
+		const changes = req.body;
+
+		if (req.file) {
+			changes.image = req.file.buffer.toString("base64");
+		}
+		try {
+			await productsRepo.update(req.params.id, changes);
+		} catch (err) {
+			return res.send("Could not find item");
+		}
+
+		res.redirect("/admin/products/");
 	}
 );
 export default router;

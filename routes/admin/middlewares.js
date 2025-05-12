1f593d38b4154ebbea6a175541f98eb901db245a
dio067 <yousefnas06@gmail.com>
2025-05-12 13:19:24 +0400

    fix: middleware HandleError

---
+++ routes/admin/middlewares.js
@@ -0,0 +1,26 @@
+import { validationResult } from "express-validator";
+
+export default {
+	handleErrors(templateFunc, dataCb) {
+		return async (req, res, next) => {
+			const errors = validationResult(req);
+
+			if (!errors.isEmpty()) {
+				let data = {};
+				if (dataCb) {
+					data = await dataCb(req);
+				}
+				return res.send(templateFunc({ errors, ...data }));
+			}
+
+			next();
+		};
+	},
+	requireAuth(req, res, next) {
+		if (!req.session.userId) {
+			return res.redirect("/signin");
+		}
+
+		next();
+	},
+};
